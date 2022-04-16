a0be963d5701e8f5349022739b9122b0e287c577
Janderson Azevedo <janderson.na.ja@gmail.com>
2022-04-16 18:59:48 -0300

    feat: Criação da função de deletar funcionario

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,6 @@ app.get("/funcionario/:id", (req, res) => {
   const SQL = "SELECT * FROM funcionarios WHERE id_funcionario = ?";
   const { id } = req.params;
 
-
   db.query(SQL, id, (err, result) => {
     if (err) console.log(err);
     else res.send(result);
@@ -60,33 +59,41 @@ app.get("/funcionario/:id", (req, res) => {
 });
 
 app.put("/funcionario/editar", (req, res) => {
-    const { id_funcionario } = req.body;
-    const { nome } = req.body;
-    const { email } = req.body;
-    const { telefone } = req.body;
-    const { cpf } = req.body;
-    const { data_nascimento } = req.body;
-    const { endereco } = req.body;
-    const { cargo } = req.body;
-    const { salario } = req.body;
-  
-    const SQL =
-      "UPDATE funcionarios SET nome = ?, email = ?, telefone = ?, cpf = ?,data_nascimento = ?, endereco = ?, cargo = ?, salario = ? WHERE id_funcionario = ?";
-    const binds = [
-      nome,
-      email,
-      telefone,
-      cpf,
-      data_nascimento,
-      endereco,
-      cargo,
-      salario,
-      id_funcionario,
-    ];
-    db.query(SQL, binds, (err, res) => {
-      if (err) console.log(err);
-    });
+  const { id_funcionario } = req.body;
+  const { nome } = req.body;
+  const { email } = req.body;
+  const { telefone } = req.body;
+  const { cpf } = req.body;
+  const { data_nascimento } = req.body;
+  const { endereco } = req.body;
+  const { cargo } = req.body;
+  const { salario } = req.body;
+
+  const SQL =
+    "UPDATE funcionarios SET nome = ?, email = ?, telefone = ?, cpf = ?,data_nascimento = ?, endereco = ?, cargo = ?, salario = ? WHERE id_funcionario = ?";
+  const binds = [
+    nome,
+    email,
+    telefone,
+    cpf,
+    data_nascimento,
+    endereco,
+    cargo,
+    salario,
+    id_funcionario,
+  ];
+  db.query(SQL, binds, (err, res) => {
+    if (err) console.log(err);
   });
+});
+
+app.delete("/funcionario/deletar/:id", (req, res) => {
+  const { id } = req.params;
+  const SQL = "DELETE FROM funcionarios WHERE id_funcionario = ?";
+  db.query(SQL, id, (err, res) => {
+    if (err) console.log(err);
+  });
+});
 
 app.listen(3001, () => {
   console.log("rodando servidor...");
--- frontend/src/pages/list-funcionarios.jsx
+++ frontend/src/pages/list-funcionarios.jsx
@@ -1,30 +1,48 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import api from "../services/api";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ListFuncionarios = () => {
+  const navigate = useNavigate();
+  const [funcionarios, setFuncionarios] = useState([]);
 
-    const [funcionarios, setFuncionarios] = useState([]);
-    useEffect(() => {
-        api.get("/funcionarios")
-        .then(({data}) => {setFuncionarios(data)})
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  const fetchFuncionario = useCallback(() => {
+    api.get("/funcionarios").then(({ data }) => {
+      setFuncionarios(data);
+    });
+    console.log("teste")
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[funcionarios]); 
+
+  
+  const deletarFuncionario = (id_funcionario, nome) => {
+    if (window.confirm(`Tem certeza que deseja deletar o funcionário ${nome} ?`) ){
+      api.delete(`/funcionario/deletar/${id_funcionario}`);
+      fetchFuncionario();
+    }
+
+  };
+  
+  useEffect(() => {
+    fetchFuncionario();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
 
   return (
     <div>
-        <table border="1">
-          <th>Nome</th>
-          <th>E-mail</th>
-          <th>Telefone</th>
-          <th>CPF</th>
-          <th>Data de nascimento</th>
-          <th>Endereço</th>
-          <th>Cargo</th>
-          <th>Salário (R$) </th>
-          <th>Ações</th>
-      
+      <table border="1">
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Telefone</th>
+        <th>CPF</th>
+        <th>Data de nascimento</th>
+        <th>Endereço</th>
+        <th>Cargo</th>
+        <th>Salário (R$) </th>
+        <th>Ações</th>
+
         {funcionarios?.map((funcionario) => (
           <tr>
             <td>{funcionario.nome}</td>
@@ -35,10 +53,22 @@ const ListFuncionarios = () => {
             <td>{funcionario.endereco}</td>
             <td>{funcionario.cargo}</td>
             <td>{funcionario.salario}</td>
-            <td><Link to={`funcionario/editar/${funcionario.id_funcionario}`} key={funcionario.id_funcionario} >Editar</Link></td>
+            <td>
+              <Link
+                to={`funcionario/editar/${funcionario.id_funcionario}`}
+                key={funcionario.id_funcionario}
+              >
+                Editar
+              </Link>
+              <button
+                onClick={() => {deletarFuncionario(funcionario.id_funcionario, funcionario.nome)}}
+              >
+                Deletar
+              </button>
+            </td>
           </tr>
         ))}
-        </table>
+      </table>
     </div>
   );
 };
